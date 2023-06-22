5d7a33a1cc3f5e401713de4174a49627f141b048
Safoah91 <lampteyanastasia@gmail.com>
2023-06-22 12:19:29 +0000

    blogs schema

---
+++ src/database/models/blogs/index.js
@@ -0,0 +1,24 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+const BlogsSchema = new Schema(
+  {
+    title: { type: String, required: [true, "title required"] },
+    body: { type: String, required: [true, "blog required"] },
+    published: { type: String },
+    publishedAt: { type: Date },
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: "category",
+      required: [true, "category required"],
+    },
+    userID: {
+      type: mongoose.Types.ObjectId,
+      ref: "users",
+      required: [true, "user id required"],
+    },
+  },
+  { timestamps: true }
+);
+
+const BlogsModel = model("blogs", BlogsSchema);
+module.exports = BlogsModel;
