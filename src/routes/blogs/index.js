a073e6ff64fae824defad992a31479e18731f7c5
Safoaho1 <lampteyanastasia@gmail.com>
2023-06-25 01:00:08 +0000

    add blog route

--- src/routes/blogs/index.js
+++ src/routes/blogs/index.js
@@ -1,10 +1,14 @@
-const { blogs, createBlog } = require("../../controllers/blogs");
+const { blogs, createBlog, publishedBlogs, draftedBlogs, userBlogs } = require("../../controllers/blogs");
 
 const express = require("express");
+const verifyToken = require("../../middleware/verifyToken");
 const router = express.Router();
 
 router.route("/").get(blogs);
-router.route("/create").post(createBlog);
+router.route("/published").get(publishedBlogs);
+router.get("/user-drafted-blogs", verifyToken, draftedBlogs);
+router.get("/user-blogs", verifyToken, userBlogs);
+router.post("/create", verifyToken, createBlog);
 
 const blogRoutes = router;
 module.exports = blogRoutes;
