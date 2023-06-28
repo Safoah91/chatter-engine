0ce7af12cb172be1df71e010ba1fdb2d7c012226
Safoaho1 <lampteyanastasia@gmail.com>
2023-06-28 18:01:51 +0000

    add endpoints

--- src/routes/blogs/index.js
+++ src/routes/blogs/index.js
@@ -1,15 +1,17 @@
-const { blogs, singleBlog, singleUserBlog, createBlog, publishedBlogs, draftedBlogs, userBlogs } = require("../../controllers/blogs");
+const { blogs, singleBlog, singleUserBlog, createBlog, publishedBlogs, draftedBlogs, userBlogs, updateBlog } = require("../../controllers/blogs");
 
 const express = require("express");
 const verifyToken = require("../../middleware/verifyToken");
 const router = express.Router();
 
 router.route("/").get(blogs);
+router.route("/blog/").get(singleBlog);
 router.route("/published").get(publishedBlogs);
 router.get("/user-drafted-blogs", verifyToken, draftedBlogs);
 router.get("/user-blogs", verifyToken, userBlogs);
 router.get("/user-blogs/blog/", verifyToken, singleUserBlog);
-router.post("/create", verifyToken, createBlog);
+router.post("/blog/create", verifyToken, createBlog);
+router.put("/blog/update/", verifyToken, updateBlog);
 
 const blogRoutes = router;
 module.exports = blogRoutes;
